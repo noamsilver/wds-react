bbda3cf45a5c652017abda8eefbffa02b3fdabb7
Noam Silver <noamsilver@gmail.com>
2018-01-16 20:10:49 +0200

    Working on menu, now only the appropriate actions are shown.

--- src/js/components/Menu.js
+++ src/js/components/Menu.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
+import ViewStore from '../flux/ViewStore';
+import Actions from '../flux/Actions';
+import constants from '../constants';
 
 class Menu extends Component {
   constructor(props) {
-    super()
+    super();
+    this.state = {
+      current: ViewStore.getCurrentView()
+    }
+    this.updateMenu = this.updateMenu.bind(this);
+  }
+  componentWillMount() {
+    ViewStore.addChangeListener(this.updateMenu);
+  }
+  componentWillUnmount() {
+    ViewStore.removeChangeListener(this.updateMenu);
+  }
+  updateMenu() {
+    this.setState({ current: ViewStore.getCurrentView() })
   }
   render() {
+    const current = this.state.current;
     return (
       <div>
-        <h1>myLocations</h1>
+        <div id="name">myLocations</div>
         <div id="actions">
-          <span>View</span>
-          <span>Edit</span>
-          <span>Add</span>
-          <span>Remove</span>
+          {(current.item === false && current.edit === false) && <span>New</span>}
+          {current.item === true && <span>Edit</span>}
+          {current.item === true && <span>Remove</span>}
         </div>
+        {(current.item === true || current.edit === true) && <div className="close">Close</div>}
       </div>
     );
   }
