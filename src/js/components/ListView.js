56547edd8db1e2e6eebbc7de36b472ebf6656a50
Noam Silver <noamsilver@gmail.com>
2018-01-28 14:54:24 +0200

    Added filter options

--- src/js/components/ListView.js
+++ src/js/components/ListView.js
@@ -10,11 +10,15 @@ class ListView extends Component {
   constructor(props) {
     super(props);
     const params = this.props.match.params;
+    const isLocation = params.view === constants.LOCATIONS;
+    const list = isLocation ? DataStore.getAllLocations() : DataStore.getAllCategories();
     this.state = {
       view: params.view,
       sortAsc: true,
       groupCategories: false,
-      list: params.view === constants.LOCATIONS ? DataStore.getAllLocations() : DataStore.getAllCategories()
+      filter: 'all',
+      currentCategories: isLocation ? this.getCurrentCategories(list).sort(this.sortByNameAcs) : undefined,
+      list
     };
     this.createList = this.createList.bind(this);
     this.updateData = this.updateData.bind(this);
@@ -22,19 +26,24 @@ class ListView extends Component {
     this.sortByName = this.sortByName.bind(this);
     this.changeSortDirection = this.changeSortDirection.bind(this);
     this.changeGrouping = this.changeGrouping.bind(this);
+    this.changeFilter = this.changeFilter.bind(this);
+    this.getCurrentCategories = this.getCurrentCategories.bind(this);
   }
   componentWillMount() {
     DataStore.addChangeListener(this.updateData);
     this.updateView(this.props.match.params.view);
   }
   componentWillReceiveProps(nextProps) {
+    const params = nextProps.match.params;
+    const isLocation = params.view === constants.LOCATIONS;
+    const list = isLocation ? DataStore.getAllLocations() : DataStore.getAllCategories();
     this.setState({
-      view: nextProps.match.params.view,
-      list: nextProps.match.params.view === constants.LOCATIONS ? DataStore.getAllLocations() : DataStore.getAllCategories()
+      view: params.view,
+      filter: isLocation ? this.state.filter : 'all',
+      currentCategories: isLocation ? this.getCurrentCategories(list).sort(this.sortByNameAcs) : undefined,
+      list
     });
-  }
-  componentWillUpdate(nextProps, nextState) {
-    this.updateView(nextProps.match.params.view);
+    this.updateView(params.view);
   }
   componentWillUnmount() {
     DataStore.removeChangeListener(this.updateData);
@@ -51,14 +60,22 @@ class ListView extends Component {
   changeGrouping() {
     this.setState({ groupCategories: !this.state.groupCategories });
   }
+  changeFilter(event) {
+    this.setState({ filter: event.target.value })
+  }
+  getCurrentCategories(list) {
+    let categoriesSet = new Set(list.map(item => item.category));
+    let categoriesNames = [];
+    categoriesSet.forEach(item => categoriesNames.push({ name: item }));
+    return categoriesNames;
+  }
   createList() {
     let newList = this.state.list;
-    if (this.props.match.params.view === constants.LOCATIONS && this.state.groupCategories) {
+    const isLocation = this.props.match.params.view === constants.LOCATIONS
+    if (isLocation && (this.state.groupCategories || this.state.filter !== 'all')) {
       let categoriesList = [];
-      let categoriesSet = new Set(newList.map(item => item.category));
-      let categoriesNames = [];
-      categoriesSet.forEach(item => categoriesNames.push({ name: item }));
-      categoriesNames.sort(this.sortByName);
+      let categoriesNames = this.state.currentCategories;
+      this.state.filter === 'all' ? categoriesNames.sort(this.sortByName) : categoriesNames = categoriesNames.filter(category => category.name === this.state.filter);
       categoriesNames.forEach(category => {
         let categoryList = newList.filter(item => item.category === category.name);
         categoryList.length > 1 && categoryList.sort(this.sortByName);
@@ -86,6 +103,15 @@ class ListView extends Component {
     }
     return 0;
   }
+  sortByNameAcs(a, b) {
+    if (a.name && b.name && a.name.toLowerCase() < b.name.toLowerCase()) {
+      return -1;
+    }
+    if (a.name && b.name && a.name.toLowerCase() > b.name.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  }
   render() {
     const nextList = this.createList();
     const params = this.props.match.params;
@@ -94,8 +120,16 @@ class ListView extends Component {
         <div className="sort-categories">
           <div onClick={this.changeSortDirection}><span>Sort</span> {this.state.sortAsc ? 'Asc' : 'Dec'}</div>
           {params.view === constants.LOCATIONS && <div onClick={this.changeGrouping}><span>Display</span> {this.state.groupCategories ? 'Grouped' : 'Ungrouped'}</div>}
+          {params.view === constants.LOCATIONS && 
+            <div onClick={this.changeFilter}><span>Filter </span> 
+              <select name="filter" value={this.state.filter} onChange={this.changeFilter}>
+                <option key="-1" value="all">All</option>
+                {this.state.currentCategories.map((item, index) => <option key={index} value={item.name}>{item.name}</option>)}
+              </select>
+            </div>
+          }
         </div>
-        <h2>{params.view === constants.LOCATIONS ? 'Locations' : 'Categories'}</h2>
+        <h2 className="title">{params.view === constants.LOCATIONS ? 'Locations' : 'Categories'}</h2>
         { nextList.length === 0 ? <div>Click New to add {params.view}</div> : nextList }
       </div>
     );
