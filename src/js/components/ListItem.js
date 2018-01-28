b907daa64c059f3d46073878c3873c090b589c3a
Noam Silver <noamsilver@gmail.com>
2018-01-29 00:32:43 +0200

    Vibrate via HTML5 vibration API

--- src/js/components/ListItem.js
+++ src/js/components/ListItem.js
@@ -3,11 +3,24 @@ import { Link } from 'react-router-dom';
 import '../../styles/list-item.css';
 
 class ListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.vibrateDevice = this.vibrateDevice.bind(this);
+  }
+  initializeVibration() {
+    // check vibration support
+    navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+  }
+  vibrateDevice() {
+    if (navigator.vibrate) {
+      navigator.vibrate(500);
+    }
+  }
   render() {
     const props = this.props;
     return (
       <div className="row">
-        <Link to={props.to}>
+        <Link to={props.to} onClick={this.vibrateDevice}>
           <div className="fill">
             {props.item.name}
             {props.isLocation && <span className="cat-name">{props.item.category}</span>}
