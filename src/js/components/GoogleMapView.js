26e2ff287d56db1c34e5ab4aeb7accb7940e6e75
Noam Silver <noamsilver@gmail.com>
2018-01-31 22:10:40 +0200

    Added an option to view locations on a google map. The map is responsive and will always fill the content area.

--- src/js/components/GoogleMapView.js
+++ src/js/components/GoogleMapView.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+import { MarkerWithLabel } from 'react-google-maps/lib/components/addons/MarkerWithLabel';
 
 class GoogleMapView extends Component {
   render() {
     return (
       <GoogleMap
-        defaultZoom={8}
+        defaultZoom={13}
         defaultCenter={{ lat: this.props.item.coordinates.lat, lng: this.props.item.coordinates.lng }}
       >
-        <Marker position={{ lat: this.props.item.coordinates.lat, lng: this.props.item.coordinates.lng }}/>
+        <MarkerWithLabel
+          position={{ lat: this.props.item.coordinates.lat, lng: this.props.item.coordinates.lng }}
+          labelAnchor={new google.maps.Point(0, 0)}
+          labelStyle={{backgroundColor: 'rgba(255, 255, 255, 0.75)', borderRadius: '10px', fontSize: "25px", padding: "10px"}}
+        >
+          <div>{this.props.item.name}</div>          
+        </MarkerWithLabel>
       </GoogleMap>
     )
   }
--- src/js/components/MapView.js
+++ src/js/components/MapView.js
@@ -1,35 +1,52 @@
 import React, { Component } from 'react';
 import GoogleMapView from './GoogleMapView';
 import DataStore from '../flux/DataStore';
+import Actions from '../flux/Actions'
 
 class MapView extends Component {
   constructor(props) {
     super(props);
-    this.state = { item: DataStore.getLocation(this.props.match.params.item) }
+    this.state = {
+      item: DataStore.getLocation(this.props.match.params.item),
+      mapHeight: this.calcHeight()
+    }
 
     this.updateView = this.updateView.bind(this);
+    this.updateHeight = this.updateHeight.bind(this);
   }
   componentWillMount() {
-    DataStore.addChangeListener(this.updateData);
     this.updateView(this.props.match.params);
   }
-
   componentWillReceiveProps(nextProps) {
     this.setState({ item: DataStore.getLocation(neztProps.match.params.item) })
     this.updateView(nextProps.match.params);
   }
+  componentDidMount() {
+    window.addEventListener('resize', this.updateHeight);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateHeight);
+  }
   updateView(params) {
     Actions.itemView(params.item);
+  }
+  calcHeight() {
+    return window.innerHeight - 265;
+  }
+  updateHeight() {
+    this.setState({ mapHeight: this.calcHeight()})
   }
   render() {
     return (
       <GoogleMapView
-        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCDTftK5de8YY-KCgdZ2tB2NfTgBWsziTI'}
-        loadingElement={<div style={{ height: `100%` }} />}
+        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDAPL1m0qlIIwrRenSSJtvx0r3FsWlMGTg'}
+        loadingElement={<div style={{ height: this.state.mapHeight, width: '100%' }} />}
         containerElement={<div id="map" className="content" />}
-        mapElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: this.state.mapHeight, width: '100%'}} />}
         item={this.state.item}
       />
     );
   }
 }
+
+export default MapView;
